32b3902aefc7a4cc1b9aa62f848ebef2527eaaea
pulkit99dev <pulkitnagpal987654321@gmail.com>
2021-06-02 21:29:49 +0530

    ng-repeat & filter(pipeline)

--- Basics/index.js
+++ Basics/index.js
@@ -1,36 +1,71 @@
 var app = angular
         .module('myApp', [])
         .controller('myController', function($scope){
-            var technologies=[
-                {
-                    name:"Java",
-                    likes: 0,
-                    dislikes: 0
-                },
-                {
-                    name:"Javascript",
-                    likes: 0,
-                    dislikes: 0
-                },
-                {
-                    name:"Flutter",
-                    likes: 0,
-                    dislikes: 0
-                },
-                {
-                    name:"React Native",
-                    likes: 0,
-                    dislikes: 0
-                }
-            ]
+                var drivers=[
+                    {
+                        name: "Lewis Hamilton",
+                        dob: new Date("November 23, 1980"),
+                        gender: 'Male',
+                        salary: 60000000.566
+                    },
+                    {
+                        name: "Vettel",
+                        dob: new Date("May 05, 1970"),
+                        gender: 'Male',
+                        salary: 20000000
+                    },
+                    {
+                        name: " Max ",
+                        dob: new Date("August 16, 1960"),
+                        gender: 'Male',
+                        salary: 18000000.345
+                    },
+                    {
+                        name: "Charles",
+                        dob: new Date("April 23, 1990"),
+                        gender: 'Male',
+                        salary: 25000000.123
+                    },
+                    {
+                        name: "Lando Norris",
+                        dob: new Date("December 21, 1998"),
+                        gender: 'Male',
+                        salary: 20000000
+                    }
+                ];
+                $scope.rowLimit = 5;
+     $scope.drivers= drivers;
 
-                $scope.technologies = technologies;
-                $scope.incrementLikes = function(technology){
-                    technology.likes++
-                }
-                $scope.incrementDislikes = function(technology){
-                    technology.dislikes
-                }
+        //     var technologies=[
+        //         {
+        //             name:"Java",
+        //             likes: 0,
+        //             dislikes: 0
+        //         },
+        //         {
+        //             name:"Javascript",
+        //             likes: 0,
+        //             dislikes: 0
+        //         },
+        //         {
+        //             name:"Flutter",
+        //             likes: 0,
+        //             dislikes: 0
+        //         },
+        //         {
+        //             name:"React Native",
+        //             likes: 0,
+        //             dislikes: 0
+        //         }
+        //     ]
+
+        //         $scope.technologies = technologies;
+        //         $scope.incrementLikes = function(technology){
+        //             technology.likes++;
+        //         }
+        //         $scope.incrementDislikes = function(technology){
+        //             technology.dislikes++;
+        //         }
 //      var countries = [
 //          {
 //             name: "USA",
